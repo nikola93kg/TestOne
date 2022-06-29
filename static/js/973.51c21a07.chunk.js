dc2c8ef8ddf1b1c6b6a93ca657cab3fb58d08ae7
Nikola Markovic <53098565+nikola93kg@users.noreply.github.com>
2022-06-29 13:06:06 +0200

    Updates

---
+++ static/js/973.51c21a07.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktest_one=self.webpackChunktest_one||[]).push([[973],{973:function(e,t,n){n.r(t),n.d(t,{default:function(){return s}});n(791);var c=n(184);var s=function(e){var t=e.img,n=e.fileName,s=e.handleClick,a=e.select;return(0,c.jsxs)("div",{className:"list-view",onClick:function(e){return s(e.target)},children:[(0,c.jsx)("img",{src:t,alt:n}),(0,c.jsx)("p",{className:"".concat(a.selected?"active":""," "),children:n})]})}}}]);
+//# sourceMappingURL=973.51c21a07.chunk.js.map
